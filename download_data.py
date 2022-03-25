79b3a272f20853f70049ce703aea91b5abad9585
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-03-24 18:22:31 -0700

    add work in prog

--- download_data.py
+++ download_data.py
@@ -1,4 +1,5 @@
 #### Standard Library Imports
+import io
 import gdown
 import zipfile
 import subprocess
@@ -12,3 +13,19 @@ from IPython.core import debugger
 breakpoint = debugger.set_trace
 
 #### Local imports
+from research_utils import io_ops
+
+## Get dirpath where to download the data
+io_dirpaths = io_ops.load_json('io_dirpaths.json')
+data_base_dirpath = io_dirpaths['data_base_dirpath']
+
+## Set scene ID that we want to download
+scene_id = "async_face_scan"
+
+## Download file
+gdrive_urls = io_dirpaths["gdrive_urls"]
+scene_url = gdrive_urls[scene_id]
+
+print("Downloading: {}".format(scene_url))
+
+gdown.download(scene_url, os.path.join(data_base_dirpath, '{}.zip'.format(scene_id)))
